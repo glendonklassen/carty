0ac9758290d1af72678e6cd1aea4a08339d39120
glendonklassen <gjklassen@protonmail.com>
2022-12-03 15:52:49 -0600

    Committing unstaged changes.

--- api/Api/MapDrawRequest.cs
+++ api/Api/MapDrawRequest.cs
@@ -2,5 +2,9 @@
 
 public class MapDrawRequest
 {
-    
+    public int? MinX { get; set; }
+    public int? MaxX { get; set; }
+    public int? MinY { get; set; }
+    public int? MaxY { get; set; }
+    public string? Type { get; set; }
 }
--- api/Api/MapSpace.cs
+++ api/Api/MapSpace.cs
@@ -2,5 +2,8 @@
 
 public class MapSpace
 {
-    
+    public int X { get; set; }
+    public int Y { get; set; }
+    public int? Z { get; set; }
+    public string? Type { get; set; }
 }
